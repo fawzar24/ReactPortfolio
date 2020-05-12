cefddbf11cd3658aa777f3ac065c5937a62ee3f8
Salah Abdullahi <fawzar@Salahs-MacBook-Pro.local>
2020-05-11 19:40:05 -0500

    contact page

--- myportfoliosite/src/App.js
+++ myportfoliosite/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="demo-big-content">
     <Layout>
-        <Header className="header-color" title="Title" scroll>
+        <Header className="header-color" title="MyPortfolio" scroll>
             <Navigation>
                 <Link to="/resume">Resume</Link>
                 <Link to="/aboutme">About Me</Link>
